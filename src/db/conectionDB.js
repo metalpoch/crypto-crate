4a99bd6d95866e607a3d015a3c293bdb3de3c170
Keiber Urbila <33231797+metalpoch@users.noreply.github.com>
2023-10-23 14:03:49 -0400

    Update conectionDB.js

--- src/db/conectionDB.js
+++ src/db/conectionDB.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://hrhlvwoabkxllmwtbcpq.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhyaGx2d29hYmt4bGxtd3RiY3BxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc4MDU0NzQsImV4cCI6MjAxMzM4MTQ3NH0.chYKgIvBowDyNgt47-Pn4ixXlhepbVBDMxygxqgFE98'
+const supabaseUrl = import.meta.env.SUPABASE_URL
+const supabaseKey = import.meta.env.SUPABASE_ANON_KEY
 
 const supabase = createClient(supabaseUrl,supabaseKey)
 
