53827e73640cb9d70d5a04746b9c1ede49344dc9
jesus <jesus_lopez1997@hotmail.com>
2023-10-26 17:15:46 -0400

    add images to supabase bucket on minting

--- src/components/MintForm.tsx
+++ src/components/MintForm.tsx
@@ -1,18 +1,55 @@
 import React, { useEffect, useRef, useState } from "react";
-
 import { create as createCrate } from "../utils/crateContract";
-import Dropzone from "./Dropzone";
+import { useDropzone } from "react-dropzone";
+import supabase from "../utils/supabase";
+import { v4 as uuidv4 } from "uuid";
+
+// import Dropzone from "./Dropzone";
+
+const thumbsContainer: React.CSSProperties = {
+  display: "flex",
+  flexDirection: "row",
+  flexWrap: "wrap",
+  marginTop: 16,
+};
+
+const thumb: React.CSSProperties = {
+  display: "inline-flex",
+  borderRadius: 2,
+  border: "1px solid #eaeaea",
+  marginBottom: 8,
+  marginRight: 8,
+  width: 100,
+  height: 100,
+  padding: 4,
+  boxSizing: "border-box",
+};
+
+const thumbInner = {
+  display: "flex",
+  minWidth: 0,
+  overflow: "hidden",
+};
+
+const img = {
+  display: "block",
+  width: "auto",
+  height: "100%",
+};
 
 export default function MintForm() {
   const { ethereum } = window;
+  const [wallet, setWallet] = useState("");
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [amount, setAmount] = useState("1");
-  const [imageUrl, setImageUrl] = useState("https://picsum.photos/600");
+  const [imageUrl, setImageUrl] = useState("");
   const [premium, setPremium] = useState(false);
   const [isSalable, setIsSalable] = useState(false);
   const [loading, setLoading] = useState(false);
   const [crate, setCrate] = useState(null);
+
+  const [files, setFiles] = useState<(File & { preview: string })[]>([]);
 
   const resultRef = useRef<null | HTMLDivElement>(null);
 
@@ -22,6 +59,8 @@ export default function MintForm() {
       if (accounts.length === 0) {
         window.location.replace("/");
         return;
+      } else {
+        setWallet(accounts[0]);
       }
     };
 
@@ -33,33 +72,42 @@ export default function MintForm() {
       setLoading(true);
       e.preventDefault();
 
-      if (
-        !title ||
-        !amount ||
-        !description ||
-        !imageUrl ||
-        Number(amount) < 1
-      ) {
+      if (!title || !amount || !description || Number(amount) < 1) {
         alert("You must fill the required fields");
         return;
       }
 
-      const newCrate = [
-        title,
-        description,
-        imageUrl,
-        premium,
-        amount,
-        isSalable,
-      ];
+      if (files[0] === undefined) {
+        alert("you must choose a valid image");
+        return;
+      }
 
-      const result = await createCrate(window.ethereum, newCrate);
-      console.log(result);
-      alert("Crate minted successfully");
-      setCrate(result);
-      clearForm();
-      if (resultRef.current) {
-        resultRef.current.scrollIntoView();
+      const { data, error } = await supabase.storage
+        .from("crates_images")
+        .upload(`${wallet}/${uuidv4()}`, files[0]);
+
+      if (data?.path) {
+        const { data: image } = supabase.storage
+          .from("crates_images")
+          .getPublicUrl(data.path);
+
+        const newCrate = [
+          title,
+          description,
+          image.publicUrl,
+          premium,
+          amount,
+          isSalable,
+        ];
+
+        const result = await createCrate(window.ethereum, newCrate);
+        console.log(result);
+        alert("Crate minted successfully");
+        setCrate(result);
+        clearForm();
+        if (resultRef.current) {
+          resultRef.current.scrollIntoView();
+        }
       }
     } catch (e) {
       console.log(e);
@@ -72,14 +120,57 @@ export default function MintForm() {
     setTitle("");
     setDescription("");
     setAmount("1");
+    setFiles([]);
   }
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: {
+      "image/*": [],
+    },
+    onDrop: (acceptedFiles) => {
+      setFiles(
+        acceptedFiles.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        )
+      );
+    },
+  });
+
+  const thumbs = files.map((file) => (
+    <div style={thumb} key={file.name}>
+      <div style={thumbInner}>
+        <img
+          src={file.preview}
+          style={img}
+          // Revoke data uri after image is loaded
+          onLoad={() => {
+            URL.revokeObjectURL(file.preview);
+          }}
+        />
+      </div>
+    </div>
+  ));
 
   return (
     <div>
       <div className="flex flex-col lg:flex-row lg:justify-center lg:items-start gap-y-24 lg:gap-x-10 mb-20 min-h-[60vh]">
         <div className="m-4">
           <div className="block mb-2 text-sm font-medium text-white">Image</div>
-          <Dropzone />
+          <section className="border border-white border-dashed rounded-lg h-[300px] lg:w-[300px]">
+            <div
+              {...getRootProps({ className: "dropzone" })}
+              className="h-[300px] w-full flex justify-center items-center"
+            >
+              <input {...getInputProps()} />
+              <div className="text-center ">
+                <p>Drag an image</p>
+                <p>or click to choose</p>
+              </div>
+            </div>
+            <aside style={thumbsContainer}>{thumbs}</aside>
+          </section>
         </div>
         <form className="p-8 flex flex-col lg:w-[400px]" onSubmit={handleForm}>
           <div className="mb-4">
