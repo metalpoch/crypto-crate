72817c3bc8f761f3ce30903f0e9865cabb720473
Poch <keiberup.dev@gmail.com>
2023-10-26 20:47:32 -0400

    fix: QR component fixed

--- src/components/QR.jsx
+++ src/components/QR.jsx
@@ -1,17 +1,5 @@
-import React from 'react';
-import QRCode from 'qrcode.react';
+import QRCode from "qrcode.react";
 
-function App() {
-    return (
-        <div>
-            <QRCode value="keiber te odio por no decirme de client:visible"
-                size={320}
-                includeMargin={true}
-                // bgColor='#FFFFFF'
-                // fgColor='#0000'
-            />
-        </div>
-    );
+export default function ({ text }) {
+  return <QRCode value={text} size={320} includeMargin={true} />;
 }
-
-export default App;
